6478170a0f2fd20cfd160cfb9bea366d8f627c20
Anderson Banihirwe <axbanihirwe@ualr.edu>
2023-06-06 14:30:59 -0600

    disable pool class

--- carbonplan_offsets_db/database.py
+++ carbonplan_offsets_db/database.py
@@ -1,3 +1,4 @@
+from sqlalchemy.pool import NullPool
 from sqlmodel import Session, create_engine
 
 from .settings import get_settings
@@ -6,7 +7,11 @@ from .settings import get_settings
 def get_session() -> Session:
     settings = get_settings()
 
-    engine = create_engine(settings.database_url, connect_args={'options': '-c timezone=utc'})
+    engine = create_engine(
+        settings.database_url,
+        connect_args={'options': '-c timezone=utc'},
+        poolclass=NullPool,
+    )
 
     with Session(engine) as session:
         yield session
