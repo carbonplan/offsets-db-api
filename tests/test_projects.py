57fb4f36d6f7209c295a3bd32c2b9acfd14c6fd9
Anderson Banihirwe <axbanihirwe@ualr.edu>
2023-09-05 14:46:09 -0700

    remove credits from project payload

--- tests/test_projects.py
+++ tests/test_projects.py
@@ -14,8 +14,6 @@ def test_get_project(test_app):
     assert isinstance(data, dict)
     assert data['project_id'] == 'ACR182'
     assert data['registry'] == 'american-carbon-registry'
-    assert isinstance(data['credits'], list)
-    assert isinstance(data['credits'][0], dict)
 
     assert 'issued' in data
     assert 'retired' in data
