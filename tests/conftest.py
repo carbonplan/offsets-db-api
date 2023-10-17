6b2cd41a7a663c6daabf750ee7b6522f3d7eec3b
Anderson Banihirwe <axbanihirwe@ualr.edu>
2023-10-16 20:48:49 -0700

    fix payload

--- tests/conftest.py
+++ tests/conftest.py
@@ -35,29 +35,34 @@ def test_app():
 @pytest.fixture(scope='session', autouse=True)
 def setup_post(test_app):
     # Define payload for POST request
-    payload = (
-        [
-            {
-                'url': 's3://carbonplan-share/offsets-db-testing-data/final/projects-augmented.parquet',
-                'category': 'projects',
-            },
-            {
-                'url': 's3://carbonplan-share/offsets-db-testing-data/final/credits-augmented.parquet',
-                'category': 'credits',
-            },
-        ],
-    )
+    payload = [
+        {
+            'url': 's3://carbonplan-share/offsets-db-testing-data/final/projects-augmented.parquet',
+            'category': 'projects',
+        },
+        {
+            'url': 's3://carbonplan-share/offsets-db-testing-data/final/credits-augmented.parquet',
+            'category': 'credits',
+        },
+    ]
+
     # Perform POST request
-    test_app.post('/files', json=payload)
+    post_response = test_app.post('/files', json=payload)
+    print(f'POST response: {post_response.json()}')
 
     # Wait until files are processed or 10-second timeout is reached
-    timeout = time.time() + 5  # 10 seconds from now
-    while True:
-        if time.time() > timeout:
-            break  # or raise an exception
+    timeout = time.time() + 20  # 10 seconds from now
+    while not time.time() > timeout:
+        response_file_1 = test_app.get(f"/files/{post_response.json()[0]['id']}")
+        response_file_2 = test_app.get(f"/files/{post_response.json()[1]['id']}")
 
         # TODO: Implement your condition check here. If it passes, break out of loop.
-        # if condition():
-        #     break
+        if (
+            response_file_1.status_code == 200
+            and response_file_2.status_code == 200
+            and response_file_1.json()['status'] == 'success'
+            and response_file_2.json()['status'] == 'success'
+        ):
+            break
 
         time.sleep(1)  # Sleep for 1 second between checks
