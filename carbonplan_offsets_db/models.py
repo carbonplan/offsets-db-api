c9b368c2e629c90045d98b09af562fb902d41ab4
Anderson Banihirwe <axbanihirwe@ualr.edu>
2023-10-21 19:22:57 -0700

    add clips to database

--- carbonplan_offsets_db/models.py
+++ carbonplan_offsets_db/models.py
@@ -9,6 +9,20 @@ from sqlmodel import BigInteger, Column, Field, SQLModel, String
 from .schemas import FileCategory, FileStatus, Pagination
 
 
+class Clip(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    project_id: str = Field(description='Project id used by registry system')
+    published_at: datetime.datetime = Field(description='Date the clip was published')
+    title: str | None = Field(description='Title of the clip')
+    url: pydantic.AnyUrl | None = Field(description='URL to the clip')
+    tags: list[str] | None = Field(
+        description='Tags associated with the clip', sa_column=Column(postgresql.ARRAY(String()))
+    )
+    notes: str | None = Field(description='Notes associated with the clip')
+    is_waybacked: bool = Field(default=False, description='Whether the clip is a waybacked clip')
+    article_type: str = Field(description='Type of clip', default='unknown')
+
+
 class File(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     url: pydantic.AnyUrl
@@ -47,6 +61,15 @@ class Project(SQLModel, table=True):
     project_url: pydantic.HttpUrl | None = Field(description='URL to project details')
 
 
+class Credit(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    project_id: str = Field(description='Project id used by registry system')
+    quantity: int = Field(description='Number of credits', sa_column=Column(BigInteger()))
+    vintage: int | None = Field(description='Vintage year of credits')
+    transaction_date: datetime.date | None = Field(description='Date of transaction')
+    transaction_type: str | None = Field(description='Type of transaction')
+
+
 # Schema for 'project' table
 project_schema = pa.DataFrameSchema(
     {
@@ -70,16 +93,6 @@ project_schema = pa.DataFrameSchema(
     }
 )
 
-
-class Credit(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
-    project_id: str = Field(description='Project id used by registry system')
-    quantity: int = Field(description='Number of credits', sa_column=Column(BigInteger()))
-    vintage: int | None = Field(description='Vintage year of credits')
-    transaction_date: datetime.date | None = Field(description='Date of transaction')
-    transaction_type: str | None = Field(description='Type of transaction')
-
-
 # Schema for 'credit' table
 credit_schema = pa.DataFrameSchema(
     {
@@ -91,6 +104,21 @@ credit_schema = pa.DataFrameSchema(
         'vintage': pa.Column(pa.Int, nullable=True, coerce=True),
         'transaction_date': pa.Column(pd.DatetimeTZDtype(tz='UTC'), nullable=True),
         'transaction_type': pa.Column(pa.String, nullable=True),
+    }
+)
+
+# Schema for 'clip' table
+clip_schema = pa.DataFrameSchema(
+    {
+        'id': pa.Column(pa.Int, nullable=False),
+        'project_id': pa.Column(pa.String, nullable=False),
+        'published_at': pa.Column(pd.DatetimeTZDtype(tz='UTC'), nullable=True),
+        'title': pa.Column(pa.String, nullable=True),
+        'url': pa.Column(pa.String, nullable=True),
+        'tags': pa.Column(pa.Object, nullable=True),
+        'notes': pa.Column(pa.String, nullable=True),
+        'is_waybacked': pa.Column(pa.Bool, nullable=True),
+        'article_type': pa.Column(pa.String, nullable=True),
     }
 )
 
