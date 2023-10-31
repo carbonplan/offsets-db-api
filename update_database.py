7d5dd21f56306115ab4e626afdc341ce8cf7146f
Anderson Banihirwe <axbanihirwe@ualr.edu>
2023-10-31 06:14:29 -0700

    seed database

---
+++ update_database.py
@@ -0,0 +1,73 @@
+import json
+import os
+import sys
+from datetime import datetime
+
+import requests
+
+
+def post_data_to_environment(env):
+    # Determine the URL based on the environment
+    url = os.environ.get(f'{env.upper()}_URL', f'https://offsets-db-{env}.fly.dev/files/')
+    print(url)
+
+    # Get the API token from environment variables
+    api_token = os.environ.get('FLY_API_TOKEN')
+
+    # Set up the headers for the request
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {api_token}',
+    }
+
+    # Determine the date for the URL
+    date_str = datetime.utcnow().strftime('%Y-%m-%d')
+
+    # Set up the data for the request
+    files = {
+        'staging': [
+            {
+                'url': 's3://carbonplan-share/offsets-db-testing-data/final/credits-augmented.parquet',
+                'category': 'credits',
+            },
+            {
+                'url': 's3://carbonplan-offsets-db/final/2023-10-31/projects-augmented.parquet',
+                'category': 'projects',
+            },
+            {
+                'url': 's3://carbonplan-share/offsets-db-testing-data/final/clips.parquet',
+                'category': 'clips',
+            },
+        ],
+        'production': [
+            {
+                'url': f's3://carbonplan-offsets-db/final/{date_str}/credits-augmented.parquet',
+                'category': 'credits',
+            },
+            {
+                'url': f's3://carbonplan-offsets-db/final/{date_str}/projects-augmented.parquet',
+                'category': 'projects',
+            },
+            {
+                'url': f's3://carbonplan-offsets-db/final/{date_str}/clips.parquet',
+                'category': 'clips',
+            },
+        ],
+    }
+
+    # Send the request
+    response = requests.post(url, headers=headers, data=json.dumps(files[env]))
+
+    # Log the response
+    if response.ok:
+        print(f'Success in {env}:', response.json())
+    else:
+        print(f'Failed in {env}:', response.text)
+
+
+if __name__ == '__main__':
+    env = sys.argv[1] if len(sys.argv) > 1 else 'staging'
+    print(f'Seeding {env} database...')
+    post_data_to_environment(env)
+    print('Done.')
