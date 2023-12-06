0617aeda5d816e1a119a9b6533d73495835ebda6
Anderson Banihirwe <axbanihirwe@ualr.edu>
2023-12-05 18:14:37 -0800

    Update release.sh script

--- release.sh
+++ release.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-echo "Python Version: $(python --version)"
-echo "Python Location: $(which python)"
 
+echo "Python Location: $(which python)"
+echo "Python Version: $(python --version)"
 
 echo "List of Python packages:"
 python -m pip list
 
-echo "alembic version: $(python -m alembic --version)"
+echo "alembic version: $(alembic --version)"
 
 # Run database migrations
 #TODO: disable alembic migrations for now to avoid breaking the build
-# echo "Running database migrations..."
-# python -m alembic upgrade head
+echo "Running database migrations..."
+alembic upgrade head
 
 echo "release complete!"
