47f57c68722c214428024bc8513d91e39cfa307e
Anderson Banihirwe <axbanihirwe@ualr.edu>
2023-12-06 02:04:35 -0800

    Update dependencies in environment files

--- release.sh
+++ release.sh
@@ -9,13 +9,8 @@ echo "Python Version: $(python --version)"
 echo "List of Python packages:"
 python -m pip list
 
-echo "alembic version: $(alembic --version)"
-
 # Run database migrations
-#TODO: disable alembic migrations for now to avoid breaking the build
 echo "Running database migrations..."
-
-
 python -m alembic upgrade head
 
 echo "release complete!"
