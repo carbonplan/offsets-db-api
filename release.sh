41eff25deb9732e4d2fb943f91800cc30dde3c49
Anderson Banihirwe <axbanihirwe@ualr.edu>
2023-12-05 18:05:01 -0800

    Disable alembic migrations and add Python version information

--- release.sh
+++ release.sh
@@ -2,15 +2,18 @@
 
 set -e
 
+echo "Python Version: $(python --version)"
+echo "Python Location: $(which python)"
+
+
 echo "List of Python packages:"
 python -m pip list
 
 echo "alembic version: $(python -m alembic --version)"
 
 # Run database migrations
-echo "Running database migrations..."
-
-echo "testing Python: $(python -c "print('Hello World')")"
-python -m alembic upgrade head
+#TODO: disable alembic migrations for now to avoid breaking the build
+# echo "Running database migrations..."
+# python -m alembic upgrade head
 
 echo "release complete!"
