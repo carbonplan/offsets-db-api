89ee7f23cad7e364675176fc5bed280828a0cda7
Anderson Banihirwe <axbanihirwe@ualr.edu>
2023-12-05 23:19:06 -0800

    Update dependencies and configuration files

--- release.sh
+++ release.sh
@@ -14,6 +14,10 @@ echo "alembic version: $(alembic --version)"
 # Run database migrations
 #TODO: disable alembic migrations for now to avoid breaking the build
 echo "Running database migrations..."
+
+echo "Enabling core Dumps"
+ulimit -c unlimited
+
 python -m alembic upgrade head
 
 echo "release complete!"
