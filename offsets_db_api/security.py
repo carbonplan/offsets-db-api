4bccaca5f4900d0971d1aa35d37703e1798c7589
Anderson Banihirwe <axbanihirwe@ualr.edu>
2023-12-08 16:07:17 -0800

    Add API key authorization to routers

--- offsets_db_api/security.py
+++ offsets_db_api/security.py
@@ -1,5 +1,6 @@
 import hmac
 
+import pydantic
 from fastapi import Depends, HTTPException, Security, status
 from fastapi.security import APIKeyHeader
 
@@ -13,14 +14,21 @@ def api_key_extractor(settings: Settings = Depends(get_settings)):
 
 
 def check_api_key(
-    api_key_header: str = Security(api_key_header), api_key: str = Depends(api_key_extractor)
+    api_key_header: str = Security(api_key_header),
+    api_key: pydantic.SecretStr = Depends(api_key_extractor),
 ):
     """Check that the API key in the header matches the API key in the settings."""
 
     if api_key_header is None:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Missing API key')
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='Missing API key. Please provide one in the X-API-KEY header.',
+        )
 
-    if hmac.compare_digest(api_key_header, api_key):
+    if hmac.compare_digest(api_key_header, api_key.get_secret_value()):
         return True
     else:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Bad API key credentials')
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='Bad API key credentials. Please check your API key in the X-API-KEY header and try again.',
+        )
