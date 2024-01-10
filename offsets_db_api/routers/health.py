8c75f1bdd798920a4de0bee3fe4802151ef6b3eb
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-01-09 17:11:01 -0800

    Add 'latest-successful-db-update' info to health router

--- offsets_db_api/routers/health.py
+++ offsets_db_api/routers/health.py
@@ -1,14 +1,49 @@
-from fastapi import APIRouter, Depends
+from collections import defaultdict
 
+from fastapi import APIRouter, Depends
+from sqlmodel import Session, col, select
+
+from ..database import get_session
+from ..logging import get_logger
+from ..models import File, FileCategory, FileStatus
 from ..security import check_api_key
 from ..settings import Settings, get_settings
 
 router = APIRouter()
+logger = get_logger()
 
 
 @router.get('/')
-def status(settings: Settings = Depends(get_settings)):
-    return {'status': 'ok', 'staging': settings.staging}
+def status(settings: Settings = Depends(get_settings), session: Session = Depends(get_session)):
+    logger.info('Received status request')
+    statement = (
+        select(File.category, File.recorded_at)
+        .where(
+            (File.status == FileStatus.success)
+            & (
+                (File.category == FileCategory.projects)
+                | (File.category == FileCategory.credits)
+                | (File.category == FileCategory.clips)
+            )
+        )
+        .order_by(col(File.recorded_at).desc())
+    )
+    results = session.exec(statement).all()
+    # Group by category and collect recorded_at dates
+    grouped_files = defaultdict(list)
+    for category, recorded_at in results:
+        grouped_files[category.value].append(recorded_at.strftime('%a, %b %d %Y %H:%M:%S UTC'))
+
+    db_latest_update = {}
+    for category, dates in grouped_files.items():
+        # get the first date
+        db_latest_update[category] = dates[0]
+
+    return {
+        'status': 'ok',
+        'staging': settings.staging,
+        'latest-successful-db-update': db_latest_update,
+    }
 
 
 @router.get('/authorized_user')
