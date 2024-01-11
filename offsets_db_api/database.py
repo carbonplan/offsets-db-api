352b36e71b14fbac92d3615c9a9e47367212643a
Anderson Banihirwe <13301940+andersy005@users.noreply.github.com>
2024-01-10 20:13:10 -0800

    Fix charts: use pandas to compute aggregations (#85)

--- offsets_db_api/database.py
+++ offsets_db_api/database.py
@@ -1,13 +1,26 @@
+from collections.abc import Generator
+
 from sqlmodel import Session, create_engine
 
 from .settings import get_settings
 
+# https://github.com/tiangolo/full-stack-fastapi-postgresql/issues/104#issuecomment-586466934
+DB_POOL_SIZE = 100
+WEB_CONCURRENCY = 2
+POOL_SIZE = max(DB_POOL_SIZE // WEB_CONCURRENCY, 5)
+
 
 def get_engine(*, database_url: str):
-    return create_engine(database_url, connect_args={'options': '-c timezone=utc'})
+    return create_engine(
+        database_url,
+        connect_args={'options': '-c timezone=utc'},
+        pool_size=POOL_SIZE,
+        max_overflow=0,
+        pool_pre_ping=True,
+    )
 
 
-def get_session() -> Session:
+def get_session() -> Generator[Session, None, None]:
     settings = get_settings()
     engine = get_engine(database_url=settings.database_url)
     with Session(engine) as session:
