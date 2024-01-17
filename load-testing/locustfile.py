0d2fa9c9c61fae906689e1a87da0d183f530d0b2
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-01-16 21:42:16 -0800

    Refactor load-testing/locustfile.py to improve API requests

--- load-testing/locustfile.py
+++ load-testing/locustfile.py
@@ -11,25 +11,31 @@ class ApiUser(FastHttpUser):
         self.headers = {'X-API-KEY': os.environ['OFFSETS_DB_API_KEY']}
 
     @task
-    def health_check(self):
-        self.client.get('/health/database', headers=self.headers, debug_stream=sys.stderr)
-
-    @task
     def get_projects(self):
-        self.client.get('/projects/', headers=self.headers, debug_stream=sys.stderr)
-
-    @task
-    def list_credits(self):
-        self.client.get('/credits/', headers=self.headers, debug_stream=sys.stderr)
-
-    @task
-    def projects_by_listing_date(self):
         self.client.get(
-            '/charts/projects_by_listing_date', headers=self.headers, debug_stream=sys.stderr
+            '/projects/?sort=-issued&current_page=1&per_page=100&registry=verra&registry=gold-standard&registry=global-carbon-council&registry=american-carbon-registry&registry=climate-action-reserve&registry=art-trees&category=agriculture&category=biochar&category=cookstove&category=energy-efficiency&category=forest&category=fuel-switching&category=ghg-management&category=land-use&category=mine-methane&category=renewable-energy&category=transportation&category=unknown',
+            headers=self.headers,
+            debug=sys.stderr,
+        )
+
+    @task
+    def get_clips(self):
+        self.client.get(
+            '/clips/?current_page=1&per_page=200&sort=-date', headers=self.headers, debug=sys.stderr
+        )
+
+    @task
+    def credits_by_category(self):
+        self.client.get(
+            '/charts/credits_by_category/?registry=verra&registry=gold-standard&registry=global-carbon-council&registry=american-carbon-registry&registry=climate-action-reserve&registry=art-trees&category=agriculture&category=biochar&category=cookstove&category=energy-efficiency&category=forest&category=fuel-switching&category=ghg-management&category=land-use&category=mine-methane&category=renewable-energy&category=transportation&category=unknown',
+            headers=self.headers,
+            debug=sys.stderr,
         )
 
     @task
     def credits_by_transaction_date(self):
         self.client.get(
-            '/charts/credits_by_transaction_date', headers=self.headers, debug_stream=sys.stderr
+            '/charts/credits_by_transaction_date/VCS902?freq=Y&current_page=1&per_page=200',
+            headers=self.headers,
+            debug=sys.stderr,
         )
