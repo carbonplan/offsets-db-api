78a623e4cea714dc5e8447165ecdc5bcd6563d72
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-01-17 17:15:46 -0800

    Add validation for category and project in clips

--- tests/test_clips.py
+++ tests/test_clips.py
@@ -3,6 +3,13 @@ def test_get_clips(test_app):
     assert response.status_code == 200
     assert isinstance(response.json()['data'], list)
 
+    # For each clip, make sure category and project are present and not empty
+    for clip in response.json()['data']:
+        assert isinstance(clip['projects'], list)
+        for project in clip['projects']:
+            assert project['project_id'] is not None
+            assert project['category'] is not None
+
 
 def test_get_filtered_clips(test_app):
     response = test_app.get('/clips/?sort=-date&date_from=2024-01-01')
