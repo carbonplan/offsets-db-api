c9ba0223f2d9fd57867a1d102336898985cb00b1
Anderson Banihirwe <axbanihirwe@ualr.edu>
2026-02-09 10:15:00 -0800

    add `--bind 0.0.0.0:8000` to your gunicorn command in the Dockerfile

--- entrypoint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-exec gunicorn -w "${OFFSETS_DB_WEB_CONCURRENCY:-2}" -t 600 -k uvicorn.workers.UvicornWorker offsets_db_api.main:app --config gunicorn_config.py --access-logfile '-' --error-logfile '-'
